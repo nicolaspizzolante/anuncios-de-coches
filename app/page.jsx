ef75aaddca2d994057d584bf1c19f6453cb30aea
nicolaspizzolante <nicopizzolante@gmail.com>
2025-03-24 14:09:36 -0300

    Controlo errores y agrego el componente Alert

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,42 @@
-import AnnouncementsTable from '@/components/AnnouncementsTable'
+import AnnouncementsTable from '@/components/AnnouncementsTable';
 import ThemeToggle from '@/components/ThemeToggle';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+
+const fetchAnnouncements = async () => {
+  try {
+    const res = await fetch('https://arval-uat-euw-appservice-portalapi.azurewebsites.net/api/Announcements/5?pageNumber=1&pageSize=500', { next: { revalidate: 3600 } });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const responseData = await res.json();
+
+    return responseData.announcements.announcements.map(item => ({
+      id: item.id,
+      make: item.make,
+      model: item.model,
+      trim: item.trim,
+      salePriceGross: item.salePriceGross,
+      firstRegistrationDate: item.firstRegistrationDate,
+      mileage: item.details.mileage,
+      gearbox: item.details.gearbox
+    }));
+  } catch (error) {
+    console.error('Error fetching announcements:', error);
+    throw error;
+  }
+};
 
 const HomePage = async () => {
-  const res = await fetch('https://arval-uat-euw-appservice-portalapi.azurewebsites.net/api/Announcements/5?pageNumber=1&pageSize=500', { next: { revalidate: 3600 } });
-  const responseData = await res.json();
+  let filteredAnnouncements = [];
+  let errorMessage = '';
 
-  const filteredAnnouncements = responseData.announcements.announcements.map(item => ({
-    id: item.id,
-    make: item.make,
-    model: item.model,
-    trim: item.trim,
-    salePriceGross: item.salePriceGross,
-    firstRegistrationDate: item.firstRegistrationDate,
-    mileage: item.details.mileage,
-    gearbox: item.details.gearbox
-  }));
+  try {
+    filteredAnnouncements = await fetchAnnouncements();
+  } catch (error) {
+    errorMessage = error.message;
+  }
 
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 w-full max-w-[1800px] mx-auto">
@@ -23,13 +45,21 @@ const HomePage = async () => {
         <h1 className='text-3xl font-bold text-center sm:text-left'>
           Anuncios de coches
         </h1>
-        <div className="w-full overflow-x-auto">
-          <AnnouncementsTable data={filteredAnnouncements} />
-        </div>
+
+        {errorMessage ? (
+          <Alert variant="destructive">
+            <AlertTitle>Hubo un error:</AlertTitle>
+            <AlertDescription>{errorMessage}</AlertDescription>
+          </Alert>
+        ) : (
+          <div className="w-full overflow-x-auto">
+            <AnnouncementsTable data={filteredAnnouncements} />
+          </div>
+        )}
+
       </main>
     </div>
   );
-}
+};
 
-
-export default HomePage
+export default HomePage;
